326ebc051c2252918619442b72df1e1cb3880aba
Maxwell Snyder <55818423+maxsspot@users.noreply.github.com>
2023-01-26 15:35:20 -0500

    Update access.js

--- access.js
+++ access.js
@@ -1,5 +1,1 @@
-function bigfont () {
-  localStorage.setItem ("largetext","true");
-  document.getElementsByTagName("button").style.fontSize = "35px";
-  document.getElementsByTagName("p").style.fontSize = "40px";
-}
+
